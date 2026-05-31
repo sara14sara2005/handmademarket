e358c6b903fc1a003c363d30e0575c1c48d4d97e
sarra <sara14.sara2005@gmail.com>
2026-05-31 18:44:34 +0200

    update backend

--- backend.js
+++ backend.js
@@ -65,9 +65,8 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "welcomepage.html"));
 });
 
-/* 🚀 START */
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("Server running on port " + PORT + " 🚀");
+    console.log("Server running 🚀");
 });

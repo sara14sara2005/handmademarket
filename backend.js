7efc97c82d16106f5ea099e3ff95af582e198e83
sarra <sara14.sara2005@gmail.com>
2026-05-31 16:49:57 +0200

    move pages to public

--- backend.js
+++ backend.js
@@ -66,7 +66,7 @@ app.get("/", (req, res) => {
 });
 
 /* 🚀 START */
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log("Server running on port " + PORT + " 🚀");

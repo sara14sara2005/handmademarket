6fcbfe0c691dc1e1d12f8523ee6a5330319eec93
sarra <sara14.sara2005@gmail.com>
2026-06-01 21:30:06 +0200

    update backend

--- backend.js
+++ backend.js
@@ -104,7 +104,7 @@ app.get("/orders", async (req, res) => {
 
 /* ================= HOME PAGE ================= */
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "welcomepage.html"));
+    res.sendFile(path.resolve("public/welcomepage.html"));
 });
 
 /* ================= START SERVER ================= */
